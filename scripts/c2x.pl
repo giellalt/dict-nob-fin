73bfbe70dc0e0040400aec80539ce91d8005b938
Trond Trosterud <trond.trosterud@uit.no>
2020-04-15 08:13:22 +0000

    script

---
+++ scripts/c2x.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/perl -w
+use utf8 ;
+
+# Simple script to convert csv to xml
+# For input/outpus examples, see below.
+
+
+print STDOUT "<r>\n";
+
+while (<>) 
+{
+	chomp ;
+#	my ($lemma, $POS, $trans, $trans2, $trans3) = split /\t/ ;
+	my ($lemma, $POS, $trans) = split /\t/ ;
+	print STDOUT "   <e>\n";
+	print STDOUT "      <lg>\n";
+	print STDOUT "         <l pos=\"$POS\">$lemma</l>\n";
+#	print STDOUT "         <l pos=\"$POS\" type=\"Prop\">$lemma</l>\n";
+	print STDOUT "      </lg>\n";
+	print STDOUT "      <mg>\n";
+	print STDOUT "         <tg xml:lang=\"fin\">\n";
+	print STDOUT "            <t pos=\"$POS\">$trans</t>\n";
+	print STDOUT "            <xg>\n";
+	print STDOUT "              <x></x>\n";
+	print STDOUT "              <xt></xt>\n";
+	print STDOUT "            </xg>\n";
+	print STDOUT "         </tg>\n";
+	print STDOUT "      </mg>\n";
+#	print STDOUT "      <mg>\n";
+#	print STDOUT "         <tg>\n";
+#	print STDOUT "            <t pos=\"$POS\">$trans2</t>\n";
+#	print STDOUT "         </tg>\n";
+#	print STDOUT "      </mg>\n";
+#	print STDOUT "      <mg>\n";
+#	print STDOUT "         <tg>\n";
+#	print STDOUT "            <t pos=\"$POS\">$trans3</t>\n";
+#	print STDOUT "         </tg>\n";
+#	print STDOUT "      </mg>\n";
+	print STDOUT "   </e>\n";
+}
+
+print STDOUT "</r>\n";
+
+
+
+# Example input:
+#
+# aampumakenttä	N	skytefelt
+
+
+#Target output:
+#
+#   <e src="yr">
+#      <lg>
+#         <l pos="N">aampumakenttä</l>
+#      </lg>
+#      <mg>
+#         <tg>
+#            <t pos="N" gen="x">skytefelt</t>
+#         </tg>
+#      </mg>
+#   </e>
+#
+
